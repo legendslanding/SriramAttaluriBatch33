6245f5af9d40b6cf28e7a0d748c64fa375bf1f00
legendslanding <Sriram.Attaluri@gmail.com>
2024-01-12 14:44:38 -0600

    src/.

---
+++ Baatch33/src/01-12/Department.java
@@ -0,0 +1,4 @@
+abstract class Department {
+    abstract int getDepartmentSize();
+
+}
---
+++ Baatch33/src/01-12/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle extends Shape{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public void calculateArea() {
+        double area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
