54fdaa2cabf7f44a21c8fb968989aa50d60c019a
legendslanding <Sriram.Attaluri@gmail.com>
2024-01-12 14:43:40 -0600

    src/.

---
+++ Baatch33/src/01-11/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public static void main(String[] args) {
+        Student student = new Student("Sriram Attaluri");
+        System.out.println("Student's name: " + student.getName());
+    }
+}
