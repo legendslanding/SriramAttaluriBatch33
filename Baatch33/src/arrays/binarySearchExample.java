64b7d264a6318d41b3f025e70ebbdcfe1c475126
legendslanding <Sriram.Attaluri@gmail.com>
2024-01-18 10:32:49 -0600

    src/.

---
+++ Baatch33/src/arrays/binarySearchExample.java
@@ -0,0 +1,46 @@
+package arrays;
+import java.util.Arrays;
+public class binarySearchExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 7;
+
+        System.out.println("Sorted Array: " + Arrays.toString(numbers));
+        System.out.println("Searching for element " + target);
+
+        int result = binarySearch(numbers, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+
+
+        return -1;
+    }
+}
