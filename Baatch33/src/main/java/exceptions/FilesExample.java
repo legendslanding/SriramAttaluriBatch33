892bd367d432efc29700a2c9b772cadcd67ff4fc
legendslanding <Sriram.Attaluri@gmail.com>
2024-02-09 12:51:13 -0600

    Baatch33/.

---
+++ Baatch33/src/main/java/exceptions/FilesExample.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilesExample {
+    public static void main(String[] args) {
+        String fileName = "C:\\SOP.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
